5e71bf879f7d5c7699c95b313ed8a22ce4da964e
JayM2510 <jay.mishra2510@gmail.com>
2022-11-16 00:38:57 +0530

    Cropping Image

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
 
-cap = cv2.VideoCapture(0)
-detector = HandDetector(maxHands=2)
+cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # For webcam
+detector = HandDetector(maxHands=1)  # Detecting number of hands
+
+offset = 20
 
 while True:
     success, img = cap.read()
     hands, img = detector.findHands(img)
+    if hands:
+        hand = hands[0]
+        x, y, w, h = hand['bbox']
+        imgCrop = img[y - offset: y + h + offset, x - offset:x + w + offset]
+        cv2.imshow("ImageCrop", imgCrop)
+
     cv2.imshow("Image", img)
     cv2.waitKey(1)
