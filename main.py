4ae0fe53267739ccef21048160cb95795badcd13
JayM2510 <jay.mishra2510@gmail.com>
2022-11-15 23:22:42 +0530

    Finding Hand

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import cv2
+from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
+detector = HandDetector(maxHands=2)
 
 while True:
     success, img = cap.read()
+    hands, img = detector.findHands(img)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
