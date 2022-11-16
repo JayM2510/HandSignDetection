db786d2703a431acf1b87d2c5ea6dab1b99628be
JayM2510 <jay.mishra2510@gmail.com>
2022-11-16 23:09:16 +0530

    Data Collection

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ import cv2
 from cvzone.HandTrackingModule import HandDetector
 import numpy as np
 import math
+import time
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # For webcam
 detector = HandDetector(maxHands=1)  # Detecting number of hands
 
 offset = 20
 imgSize = 300
+
+folder = "Data/A"
+counter = 0
 
 while True:
     success, img = cap.read()
@@ -32,8 +36,20 @@ while True:
             wGap = math.ceil((imgSize - wCal) / 2)
             imgWhite[:, wGap:wCal + wGap] = imgResize
 
+        else:
+            k = imgSize / w
+            hCal = math.ceil(k * h)
+            imgResize = cv2.resize(imgCrop, (imgSize, hCal))
+            imgResizeShape = imgResize.shape
+            hGap = math.ceil((imgSize - hCal) / 2)
+            imgWhite[hGap:hCal + hGap, :] = imgResize
+
         cv2.imshow("ImageCrop", imgCrop)
         cv2.imshow("ImageWhite", imgWhite)
 
     cv2.imshow("Image", img)
-    cv2.waitKey(1)
+    key = cv2.waitKey(1)
+    if key == ord("s"):
+        counter += 1
+        cv2.imwrite(f'{folder}/Image_{time.time()}.jpg', imgWhite)
+        print(counter)
