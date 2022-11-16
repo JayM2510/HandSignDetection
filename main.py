a6fa2fba303aade3723da93b7f516388ed0d07ba
JayM2510 <jay.mishra2510@gmail.com>
2022-11-16 21:50:19 +0530

    Custom White Image added

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import cv2
 from cvzone.HandTrackingModule import HandDetector
+import numpy as np
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)  # For webcam
 detector = HandDetector(maxHands=1)  # Detecting number of hands
 
 offset = 20
+imgSize = 300
 
 while True:
     success, img = cap.read()
@@ -12,8 +14,12 @@ while True:
     if hands:
         hand = hands[0]
         x, y, w, h = hand['bbox']
+
+        imgWhite = np.ones((imgSize, imgSize), np.vint8)*255
+
         imgCrop = img[y - offset: y + h + offset, x - offset:x + w + offset]
         cv2.imshow("ImageCrop", imgCrop)
+        cv2.imshow("ImageWhite", imgWhite)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
